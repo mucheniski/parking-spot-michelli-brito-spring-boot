02518664b9eff531180d2c4e68c584b516171a7f
DMZ1021 <diegomucheniski.gft@sulamerica.com.br>
2022-12-30 14:05:21 -0300

    criacao do dto

---
+++ src/main/java/com/example/parkingspot/dtos/ParkingSpotDTO.java
@@ -0,0 +1,36 @@
+package com.example.parkingspot.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+
+@Data
+public class ParkingSpotDTO {
+
+    @NotBlank
+    private String parkingSpotNumber;
+
+    @NotBlank
+    @Size(max = 7)
+    private String licencePlateCar;
+
+    @NotBlank
+    private String brandCar;
+
+    @NotBlank
+    private String modelCar;
+
+    @NotBlank
+    private String colorCar;
+
+    @NotBlank
+    private String responsibleName;
+
+    @NotBlank
+    private String apartment;
+
+    @NotBlank
+    private String block;
+
+}
